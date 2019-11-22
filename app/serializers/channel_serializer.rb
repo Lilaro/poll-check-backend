043a33b24ae93a98628577f2ecbf2a1bcef5b4f6
Lilaro <larochelle.lisa@gmail.com>
2019-11-22 12:04:51 -0500

    Add ids to serializer attributes

--- app/serializers/channel_serializer.rb
+++ app/serializers/channel_serializer.rb
@@ -1,6 +1,6 @@
 class ChannelSerializer
   include FastJsonapi::ObjectSerializer
-  attributes :name
+  attributes :id, :name
   has_many :messages
   has_many :users, through: :messages
   has_many :poll_sites, through: :messages
